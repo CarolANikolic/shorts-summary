4338d9a4aaec67d4a79e8101d726b565ef5fad20
Carol A. Nikolic <carolnikolic@gmail.com>
2023-09-15 21:44:57 +0200

    feat(server): create script to initiate server and create route

--- server/index.js
+++ server/index.js
@@ -1,2 +1,11 @@
 import cors from 'cors'
 import express from 'express'
+
+const app = express();
+app.use(cors());
+
+app.get("/summary/:id", (request, response) => {
+    response.send("Video id:" + request.params.id)
+})
+
+app.listen(3333, () => console.log("test"))
