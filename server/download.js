6db7dd0d0eff8f3c7c1a7047f0810a96e6c81370
Carol A. Nikolic <carolnikolic@gmail.com>
2023-09-16 14:10:21 +0200

    feat(server): make provisory download video function

---
+++ server/download.js
@@ -0,0 +1,22 @@
+import ytdl from "ytdl-core"
+import fs from "fs"
+
+export const download = (videoId) => {
+    const videoURL = "http://www.youtube.com/shorts/" + videoId;
+
+    ytdl(videoId, {quality: "lowestaudio", filter: "audioonly"}).on
+    ("info", (info) => {
+        const seconds = info.formats[0].approxDurationMs / 1000;
+        
+        if (seconds > 60) {
+            throw new Error("The duration of this video is higher than 60 seconds.")
+        }
+    }
+    ).on("end", () => {
+        console.log("Video downloded.")
+    }
+    ).on("error", (error) => {
+        console.log("Failed to download the video. Error:", error);
+    }
+    ).pipe(fs.createWriteStream("./tmp/audio.mp4"))
+}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
-import cors from 'cors'
-import express from 'express'
+import cors from "cors"
+import express from "express"
+import { download } from "./download.js"
 
 const app = express();
 app.use(cors());
 
 app.get("/summary/:id", (request, response) => {
-    response.send("Video id:" + request.params.id)
+    download(request.params.id)
+    response.json({ result: "Video downloaded with success!" })
 })
 
-app.listen(3333, () => console.log("test"))
+app.listen(3333, () => console.log("Test port 3333."))
